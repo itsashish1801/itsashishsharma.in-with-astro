b20d48dd2ccd4049b2f83d39bdf6a104d4fb5f2d
Ashish Sharma <itsashishsharma1801@gmail.com>
2023-03-09 10:33:43 +0530

    Add custom colors.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,77 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    fontSize: {
+      xs: ['0.8125rem', { lineHeight: '1.5rem' }],
+      sm: ['0.875rem', { lineHeight: '1.5rem' }],
+      base: ['1rem', { lineHeight: '1.75rem' }],
+      lg: ['1.125rem', { lineHeight: '1.75rem' }],
+      xl: ['1.25rem', { lineHeight: '2rem' }],
+      '2xl': ['1.5rem', { lineHeight: '2rem' }],
+      '3xl': ['1.875rem', { lineHeight: '2.25rem' }],
+      '4xl': ['2rem', { lineHeight: '2.5rem' }],
+      '5xl': ['3rem', { lineHeight: '3.5rem' }],
+      '6xl': ['3.75rem', { lineHeight: '1' }],
+      '7xl': ['4.5rem', { lineHeight: '1' }],
+      '8xl': ['6rem', { lineHeight: '1' }],
+      '9xl': ['8rem', { lineHeight: '1' }],
+    },
+
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        white: 'var(--color-white)',
+        black: 'var(--color-black)',
+        primary: 'var(--color-primary)',
+        'primary-dark': 'var(--color-primary-dark)',
+        secondary: 'var(--color-secondary)',
+        'secondary-dark': 'var(--color-secondary-dark)',
+        tertiary: 'var(--color-tertiary)',
+        'tertiary-dark': 'var(--color-tertiary-dark)',
+
+        gray: {
+          100: 'var(--color-gray-100)',
+          200: 'var(--color-gray-200)',
+          300: 'var(--color-gray-300)',
+          350: 'var(--color-gray-350)',
+          400: 'var(--color-gray-400)',
+          500: 'var(--color-gray-500)',
+          600: 'var(--color-gray-600)',
+          700: 'var(--color-gray-700)',
+          800: 'var(--color-gray-800)',
+          900: 'var(--color-gray-900)',
+        },
+        slate: {
+          500: 'var(--color-slate-500)',
+          600: 'var(--color-slate-600)',
+        },
+        team: {
+          unknown: 'var(--color-team-unknown)',
+          current: 'var(--color-team-current)',
+          yellow: 'var(--color-team-yellow)',
+          blue: 'var(--color-team-blue)',
+          red: 'var(--color-team-red)',
+        },
+        yellow: {
+          500: 'var(--color-yellow-500)',
+          '500-inverted': 'var(--color-yellow-500-inverted)',
+        },
+        blue: {
+          100: 'var(--color-blue-100)',
+          500: 'var(--color-blue-500)',
+        },
+        red: {
+          500: 'var(--color-red-500)',
+        },
+        green: {
+          100: 'var(--color-green-100)',
+          500: 'var(--color-green-500)',
+          600: 'var(--color-green-600)',
+        },
+      },
+    },
+  },
+  plugins: [],
+};
